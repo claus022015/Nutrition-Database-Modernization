36d60f0f2bfa17d43d05362d3309539df31c216b
claus022015 <zoica.crisan@gmail.com>
2015-06-09 14:53:54 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,9 @@ master_doc = 'index'
 
 # General information about the project.
 
+# Project information. Label goes into left nav title block
+project = 'USDA Nutrition Database'
+
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
