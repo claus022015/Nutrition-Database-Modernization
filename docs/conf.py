9e3ba7939cf62a0d4d027e7fca16603599eaefb6
claus022015 <zoica.crisan@gmail.com>
2015-09-16 22:15:27 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,12 +45,6 @@ extensions = [
               'sphinxcontrib.bibtex',
               'sphinxcontrib.images',
               'sphinxcontrib.jsdemo',
-              'numfig',
-              'numsec',
-              'natbib',
-              'figtable',
-              'singlehtml_toc',
-              'singletext'
              ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
